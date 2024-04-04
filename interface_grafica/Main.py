a6156304fad165d2d05315a011e16737e377a1e1
Gsabin0 <gsabino5667@gmail.com>
2024-04-04 16:32:34 -0300

    styles criado e atualizado, button criado e ajustado, uteis criada, variaveis recebendo uteis e mais recebendo todas as novas criadas

--- interface_grafica/Main.py
+++ interface_grafica/Main.py
@@ -6,11 +6,14 @@ from Main_calculadora import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from Variaveis import WINDOW_ICON_PATH
+from styles import setupTheme
+from button import Button, ButtonGrid
 
 
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
@@ -18,12 +21,16 @@ if __name__ == '__main__':
     app.setWindowIcon(icon)
 
     #info
-    info = Info('...')
+    info = Info('3 * 5 = 15')
     window.addToVLayout(info)
 
     # Display
     display = Display()
     window.addToVLayout(display)
+
+    # GRID
+    buttonGrid = ButtonGrid(display)
+    window.vLayout.addLayout(buttonGrid)
 
     # Executa tudo
     window.adjustFixedSize()
